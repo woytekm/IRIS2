7d70aa168f02818b0ce4171a9964fbe0b81daaa5
woytekm <woytekm@gmail.com>
2018-12-26 21:44:09 +0000

    version change for IRIS2 to 2.01

--- Code/IRIS2/include/version.h
+++ Code/IRIS2/include/version.h
@@ -1,7 +1,7 @@
 // version and compile time information
 // 
    
-#define IRIS_COMPILE_TIME "12/16/18@20:05"
-#define IRIS_VER_MAJOR 0
-#define IRIS_VER_MINOR 2
+#define IRIS_COMPILE_TIME "12/26/18@21:43"
+#define IRIS_VER_MAJOR 2
+#define IRIS_VER_MINOR 01
  
